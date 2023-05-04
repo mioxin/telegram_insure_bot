07cf06a3973acfb609fcec7b8fd23ee7e9f858e6
M. Palchuk <mrmioxin@gmail.com>
2023-05-04 18:55:09 +0600

    --amend

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,12 +11,13 @@ import (
 type Config struct {
 	Token   string
 	LogFile string
+	AccWord string
 	Deny    map[string]struct{}
 	Allow   map[string]struct{}
 }
 
 func NewConfig(config_file string) (*Config, error) {
-	var token, logFile string
+	var token, logFile, acc_word string
 	deny := make(map[string]struct{})
 	allow := make(map[string]struct{})
 	conf, err := os.ReadFile(config_file)
@@ -45,10 +46,12 @@ func NewConfig(config_file string) (*Config, error) {
 			deny[arr_str[1]] = struct{}{}
 		case "allow":
 			allow[arr_str[1]] = struct{}{}
+		case "acc_word":
+			acc_word = strings.TrimSpace(arr_str[1])
 
 		default:
 			log.Println("config: unrecognized param in the line: ", str)
 		}
 	}
-	return &Config{Token: token, LogFile: logFile, Deny: deny, Allow: allow}, nil
+	return &Config{Token: token, LogFile: logFile, Deny: deny, Allow: allow, AccWord: acc_word}, nil
 }
