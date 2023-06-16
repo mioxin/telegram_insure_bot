51ac396a9d722db4e88780ae1f8106adbc20396d
M. Palchuk <mrmioxin@gmail.com>
2023-06-16 18:11:43 +0600

    --amend

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -10,21 +10,18 @@ import (
 	"github.com/mrmioxin/gak_telegram_bot/internal/commands"
 	"github.com/mrmioxin/gak_telegram_bot/internal/config"
 	"github.com/mrmioxin/gak_telegram_bot/internal/httpclient"
-	"github.com/mrmioxin/gak_telegram_bot/internal/services"
-	"github.com/mrmioxin/gak_telegram_bot/internal/sessions"
 )
 
 const (
-	TYPE_OF_BUSNS_FILENAME string        = "vid.txt"
-	CONFIG_FILE_NAME       string        = "bot.cfg"
-	DURATION_WATCH_CONFIG  time.Duration = 3 * time.Second
+	CONFIG_FILE_NAME      string        = "bot.cfg"
+	DURATION_WATCH_CONFIG time.Duration = 3 * time.Second
 )
 
 var (
-	verbouse bool
-	debug    bool
-	token    string
-	conf     *config.Config
+	verbouse          bool
+	debug             bool
+	token, configFile string
+	conf              *config.Config
 )
 
 func init() {
@@ -32,6 +29,7 @@ func init() {
 	flag.BoolVar(&verbouse, "verbouse", false, "Output fool log to StdOut")
 	flag.BoolVar(&debug, "d", false, "Output debug info to StdOut (shorthand)")
 	flag.StringVar(&token, "t", "", "The security token for connecting to Telegram API")
+	flag.StringVar(&configFile, "c", "", "The configuration file")
 	// registers.Sessions = *sessions.NewMemSessions()
 	// registers.RegisteredCommands = make(map[string]func(mes *tgapi.Message) string)
 	// registers.RegisteredActions = make(map[string]actions.Action)
@@ -42,7 +40,11 @@ func parsConf() {
 	var err error
 	flag.Parse()
 
-	if conf, err = config.NewConfig(CONFIG_FILE_NAME); err != nil {
+	if configFile == "" {
+		configFile = CONFIG_FILE_NAME
+	}
+
+	if conf, err = config.NewConfig(configFile); err != nil {
 		log.Panic(err)
 	}
 
@@ -68,11 +70,6 @@ func main() {
 	parsConf()
 	defer botOut()
 
-	tob, err := services.NewFileTypeOfBusns(TYPE_OF_BUSNS_FILENAME)
-	if err != nil {
-		log.Fatal("<<<", TYPE_OF_BUSNS_FILENAME, ">>> ", err)
-	}
-
 	bot, err := tgapi.NewBotAPIWithClient(conf.Token, httpclient.NewHTTPClient())
 	if err != nil {
 		log.Fatal("<<<", conf.Token, ">>> ", err)
@@ -84,23 +81,6 @@ func main() {
 	isModifyConfig := make(chan any)
 	go conf.Watch(CONFIG_FILE_NAME, DURATION_WATCH_CONFIG, isModifyConfig)
 
-	insure := services.NewInsurence("ОСНС")
-	//srvs := make(map[string]sessions.Services)
-
-	c := commands.NewCommander(bot, conf, insure, sessions.NewMemSessions(), tob)
-	//go c.WatchConfig(isModifyConfig)
-
-	u := tgapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Fatal(">>> ", err)
-	}
-	time.Sleep(time.Millisecond * 500)
-	updates.Clear()
-
-	for update := range updates {
-		go c.HandlerMain(update)
-	}
+	c := commands.NewCommander(bot, conf)
+	c.Start()
 }
--- internal/commands/commander.go
+++ internal/commands/commander.go
@@ -1,19 +1,28 @@
 package commands
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	tgapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/mrmioxin/gak_telegram_bot/internal/handlers"
+	"github.com/mrmioxin/gak_telegram_bot/internal/services"
 	"github.com/mrmioxin/gak_telegram_bot/internal/sessions"
 )
 
 const (
-	WRONG_AGAIN  string = `Опять ошибка. `
-	WRONG_INPUT  string = `Введите команду или выберите её из меню.`
-	WRONG_ACCESS string = "Извините, пока доступ закрыт."
-	YES          string = "Да"
-	NO           string = "Нет"
+	TYPE_OF_BUSNS_FILENAME string = "vid.txt"
+	WRONG_AGAIN            string = `Опять ошибка. `
+	WRONG_INPUT            string = `Введите команду или выберите её из меню.`
+	WRONG_ACCESS           string = "Извините, пока доступ закрыт."
+	YES                    string = "Да"
+	NO                     string = "Нет"
 )
+
+// type IHandler interface {
+// 	Execute()
+// }
 
 type ITypeOfBusiness interface {
 	Get(vid string) (string, error)
@@ -40,28 +49,67 @@ type Commander struct {
 	TypeOfBuseness  ITypeOfBusiness
 }
 
-func NewCommander(bot *tgapi.BotAPI, conf IConfig, serv IService, ses ISessions, tob ITypeOfBusiness) *Commander {
+func NewCommander(bot *tgapi.BotAPI, conf IConfig) *Commander {
+	tob, err := services.NewFileTypeOfBusns(TYPE_OF_BUSNS_FILENAME)
+	if err != nil {
+		log.Fatal("<<<", TYPE_OF_BUSNS_FILENAME, ">>> ", err)
+	}
+
+	serv := services.NewInsurence("ОСНС")
+	//srvs := make(map[string]sessions.Services)
+	ses := sessions.NewMemSessions()
+
 	return &Commander{bot, conf, serv, ses, tob}
 }
 
-func (cmder *Commander) HandlerMain(update tgapi.Update) error {
+func (cmder *Commander) Start() {
+	u := tgapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := cmder.bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(">>> ", err)
+	}
+	time.Sleep(time.Millisecond * 500)
+	updates.Clear()
+
+	for update := range updates {
+		go cmder.handl(update)
+	}
+
+}
+
+func (cmder *Commander) handl(update tgapi.Update) error {
 	defer func() {
 		if panicVal := recover(); panicVal != nil {
 			log.Printf("recover panic in HandlerMain %v:", panicVal)
 		}
 	}()
 
+	ses, err := cmder.Sessions.GetSession(update.Message.Chat.ID)
+	if err != nil {
+		log.Printf("error HandlerMain: not found session for Message \"%v\" (user %v)", update.Message.Text, update.Message.Chat.UserName)
+		ses = sessions.NewSession(update.Message.Chat.UserName)
+		if cmder.Config.IsAccess(update.Message.Chat.UserName) {
+			ses.AccessCommand["all"] = struct{}{}
+		} else {
+			ses.AccessCommand["about"] = struct{}{}
+		}
+		cmder.Sessions.AddSession(update.Message.Chat.ID, ses)
+	}
+
 	if update.CallbackQuery != nil {
 		cmder.HandlerCallback(update)
 		return nil
 	}
 	if update.Message.IsCommand() {
-		cmder.HandlerCommand(update)
+		h := handlers.NewHandlerCommand(cmder.bot, ses, update)
+		h.Execute()
+		//cmder.HandlerCommand(update)
 	} else {
-		ses, err := cmder.Sessions.GetSession(update.Message.Chat.ID)
-		if err != nil {
-			log.Printf("error HandlerMain: not found session for Message \"%v\" (user %v)", update.Message.Text, update.Message.Chat.UserName)
-			return err
+		if ses.ActionName == "" {
+			log.Printf("error HandlerMain: not found ActionName in session of Message \"%v\" (user %v)", update.Message.Text, update.Message.Chat.UserName)
+			return fmt.Errorf("error HandlerMain: not found ActionName in session of Message \"%v\" (user %v)", update.Message.Text, update.Message.Chat.UserName)
 		}
 		switch ses.ActionName {
 		case "calc":
@@ -71,9 +119,3 @@ func (cmder *Commander) HandlerMain(update tgapi.Update) error {
 	}
 	return nil
 }
-
-// func (cmder *Commander) WatchConfig(isModify chan any) {
-// 	for range isModify {
-
-// 	}
-// }
